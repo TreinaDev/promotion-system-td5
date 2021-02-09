73d8264a2cfd0662259e3faea5ca2f3ae2fa22eb
Joao Almeida <joaorsalmeida@gmail.com>
2021-02-08 21:35:14 -0300

    WIP: aprovar promoção

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,5 +1,6 @@
 class Promotion < ApplicationRecord
   has_many :coupons
+  has_one :promotion_approval
   belongs_to :user
 
   validates :name, :code, :discount_rate, :coupon_quantity, :expiration_date, presence: true
@@ -11,6 +12,25 @@ class Promotion < ApplicationRecord
         coupons.create!(code: "#{code}-#{'%04d' % number}")
       end
     end
+  end
+
+  def approve!(approval_user)
+    PromotionApproval.create(promotion: self, user: approval_user)
+  end
+
+  def approved?
+    promotion_approval
+  end
+
+  def approved_at
+    promotion_approval&.approved_at
+
+    return nil unless promotion_approval
+    promotion_approval.approved_at
+  end
+
+  def approver
+    promotion_approval&.user
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :promotions, only: %i[index show new create] do
-    post 'generate_coupons', on: :member
+    member do
+      post 'generate_coupons'
+      post 'approve'
+    end
+    
   end
-
 
   resources :coupons, only: [] do
     post 'inactivate', on: :member
